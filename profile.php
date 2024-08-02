5f9ae0e1e5b10a9b476655aaf4dc241743569ecc
KG-coder3902 <59836235+KG-coder3902@users.noreply.github.com>
2024-08-02 16:42:33 +0530

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `studentmangement`
+ */
+
+/* `studentmangement`.`profile` */
+$profile = array(
+  array('user_id' => '10','name' => 'stevetwo','address' => 'steve2abc','email' => 'stwo@test.com','password' => 'steve2one','phone' => '1234567898','gender' => 'Male','isteacher' => 'no','profimg' => 'yordan-stoyanov-xngUPivgySY-unsplash.jpg'),
+  array('user_id' => '11','name' => 'stevethree','address' => 'stevethree213','email' => 'sthree@test.com','password' => '12345678911','phone' => '1234567896','gender' => 'Male','isteacher' => 'yes','profimg' => 'mos-design-7Bm7Lhets9Q-unsplash.jpg'),
+  array('user_id' => '19','name' => 'stevethreefour','address' => 'stevethree2134','email' => 'steve2@abctest.com','password' => 'Steve34123','phone' => '2147483647','gender' => 'Male','isteacher' => 'yes','profimg' => 'mos-design-7Bm7Lhets9Q-unsplash.jpg'),
+  array('user_id' => '8','name' => 'steve','address' => 'aaoneone','email' => 'sone@test.com','password' => 'onetwothreefour','phone' => '2147483647','gender' => 'Male','isteacher' => 'yes','profimg' => 'luke-miller-rW8ok6JpJZk-unsplash.jpg'),
+  array('user_id' => '20','name' => 'Clint Barton','address' => '2161 Titus Trace, Hamillland, NV 31593','email' => 'clintbarton@gamil.com','password' => 'Clint1234','phone' => '2147483647','gender' => 'Male','isteacher' => 'yes','profimg' => 'gantavya-bhatt-vj15n7OJOpQ-unsplash.jpg'),
+  array('user_id' => '14','name' => 'buckytwo','address' => 'stevethree213bucky2','email' => 'buckytwosteve2abc@test.com','password' => 'buckytwo123','phone' => '2147483647','gender' => 'Male','isteacher' => 'yes','profimg' => 'luke-miller-rW8ok6JpJZk-unsplash.jpg'),
+  array('user_id' => '18','name' => 'buckyeighteight','address' => 'stevethree213bucky288','email' => 'bucky88steve2abc@test.com','password' => 'Bucky81238','phone' => '2147483647','gender' => 'Male','isteacher' => 'yes','profimg' => 'luke-miller-rW8ok6JpJZk-unsplash.jpg'),
+  array('user_id' => '13','name' => 'bucky','address' => 'buckystevethree213','email' => 'bu123@test.com','password' => 'bucky2one','phone' => '2147483647','gender' => 'Male','isteacher' => 'yes','profimg' => 'gantavya-bhatt-vj15n7OJOpQ-unsplash.jpg'),
+  array('user_id' => '7','name' => 'brucetwo','address' => 'abcdefghijklmnsfda1','email' => 'atwo@test.com','password' => '112233445566','phone' => '1234567896','gender' => 'Male','isteacher' => 'no','profimg' => 'omar-ram-1eRw_E5rmB0-unsplash.jpg'),
+  array('user_id' => '16','name' => 'sfdafdsf','address' => 'sadfdsfdsa','email' => 'asdfdsf@fdsfa','password' => 'abcd123A','phone' => '1234567896','gender' => 'Male','isteacher' => 'yes','profimg' => 'omar-ram-1eRw_E5rmB0-unsplash.jpg'),
+  array('user_id' => '6','name' => 'bruceone','address' => 'abcdefghijklmn','email' => 'aone@test.com','password' => '112233445566','phone' => '1234567896','gender' => 'Male','isteacher' => 'yes','profimg' => 'marek-piwnicki-wtyODAX4rg8-unsplash.jpg'),
+  array('user_id' => '12','name' => 'admin','address' => 'admin123','email' => 'admin@test.com','password' => 'Admin1234','phone' => '1111111111','gender' => 'Male','isteacher' => 'yes','profimg' => 'marek-piwnicki-wtyODAX4rg8-unsplash.jpg'),
+  array('user_id' => '3','name' => 'tonytwo','address' => 'abcdefghijklmn2','email' => 'abcdtwo@test.com','password' => '1234567892','phone' => '1234567893','gender' => 'Male','isteacher' => 'no','profimg' => 'mos-design-7Bm7Lhets9Q-unsplash.jpg'),
+  array('user_id' => '4','name' => 'tonythree','address' => 'abcdefghijklmn3','email' => 'abcdthree@test.com','password' => '1234567894','phone' => '1234567894','gender' => 'Male','isteacher' => 'no','profimg' => 'yordan-stoyanov-xngUPivgySY-unsplash.jpg'),
+  array('user_id' => '2','name' => 'tonyone','address' => 'abcdefghijklmn1one','email' => 'abcdone@test.com','password' => '12345678911','phone' => '1234567892','gender' => 'Male','isteacher' => 'no','profimg' => 'gantavya-bhatt-vj15n7OJOpQ-unsplash.jpg'),
+  array('user_id' => '5','name' => 'tonyfour','address' => 'abcdefghijklmn4','email' => 'abcdfour@test.com','password' => '1234567895','phone' => '1234567895','gender' => 'Male','isteacher' => 'no','profimg' => 'josh-hild-iqcbtYnWX3Y-unsplash.jpg'),
+  array('user_id' => '1','name' => 'tony','address' => 'abcdefghijklmn1234','email' => 'abcd1@test.com','password' => 'abcdef123','phone' => '1234567891','gender' => 'Male','isteacher' => 'no','profimg' => 'josh-hild-iqcbtYnWX3Y-unsplash.jpg')
+);
